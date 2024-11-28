59d9d7f815f4f32c4fab240864e08f93ce154ac8
amlan-sahu <amlan9531@gmail.com>
2024-11-28 23:58:58 +0530

    add Github component with data fetching and routing

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -8,6 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import GitHub, { githubInfoLoader } from './components/Github/Github.jsx'
 
 
 // const router = createBrowserRouter([
@@ -38,7 +39,11 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
-
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<GitHub />}
+       />
     </Route>
   )
 )
